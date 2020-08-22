65b1f41980a5ca69f88c87e499c8d30f586949fb
lssuseendharlal <lssuseendharlal@gmail.com>
2020-08-22 20:11:28 +0530

    auth validation error added

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -84,85 +84,74 @@ module.exports = {
 
   Mutation: {
     signin: async (_, { email, password }) => {
-      try {
-        const { errors, isValid } = validateSigninInput(email, password);
-        console.log(errors, isValid);
-        if (!isValid) {
-          throw new UserInputError("errors", { errors });
-        }
-        const user = await User.findOne({ email });
-        if (!user) {
-          throw new UserInputError("email doesn't exists", {
-            errors: {
-              email: "Invalid credentials",
-            },
-          });
-        }
-        const isPassmatch = await bcrypt.compare(password, user.password);
-        if (!isPassmatch) {
-          throw new UserInputError("pass doesn't match", {
-            errors: {
-              email: "Invalid credentials",
-            },
-          });
-        }
-        const token = generateToken(user);
-        return {
-          ...user._doc,
-          token,
-        };
-      } catch (error) {
-        throw new UserInputError("bad input", {
-          error,
+      const { errors, isValid } = validateSigninInput(email, password);
+      console.log(errors, isValid);
+      if (!isValid) {
+        throw new UserInputError("errors", { errors });
+      }
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new UserInputError("email doesn't exists", {
+          errors: {
+            email: "Invalid credentials",
+          },
         });
       }
+      const isPassmatch = await bcrypt.compare(password, user.password);
+      if (!isPassmatch) {
+        throw new UserInputError("pass doesn't match", {
+          errors: {
+            email: "Invalid credentials",
+          },
+        });
+      }
+      const token = generateToken(user);
+      return {
+        ...user._doc,
+        token,
+      };
     },
     signup: async (
       _,
       { signupInput: { username, email, password, confirmpassword } }
     ) => {
-      try {
-        const { errors, isValid } = validateSignupInput(
-          username,
-          email,
-          password,
-          confirmpassword
-        );
-        console.log(errors, isValid);
-        if (!isValid) {
-          throw new UserInputError("errors", { errors });
-        }
-        const isUsername = await User.findOne({ username });
-        const isEmail = await User.findOne({ email });
-        if (isUsername || isEmail) {
-          if (isUsername) {
-            throw new UserInputError("username already taken", {
-              errors: {
-                username: "Username already taken",
-              },
-            });
-          }
-          if (isEmail) {
-            throw new UserInputError("email already taken", {
-              errors: {
-                email: "Email already taken",
-              },
-            });
-          }
-        }
-        const hashPass = await bcrypt.hash(password, 12);
-        const newUser = new User({
-          username,
-          email,
-          password: hashPass,
-        });
-        const result = await newUser.save();
-        const token = generateToken(result);
-        return { ...result._doc, token };
-      } catch (err) {
-        console.log(object);
-        throw new UserInputError("bad input", err);
+      const { errors, isValid } = validateSignupInput(
+        username,
+        email,
+        password,
+        confirmpassword
+      );
+      console.log(errors, isValid);
+      if (!isValid) {
+        throw new UserInputError("errors", { errors });
       }
+      const isUsername = await User.findOne({ username });
+      const isEmail = await User.findOne({ email });
+      if (isUsername || isEmail) {
+        if (isUsername) {
+          throw new UserInputError("username already taken", {
+            errors: {
+              username: "Username already taken",
+            },
+          });
+        }
+        if (isEmail) {
+          throw new UserInputError("email already taken", {
+            errors: {
+              email: "Email already taken",
+            },
+          });
+        }
+      }
+      const hashPass = await bcrypt.hash(password, 12);
+      const newUser = new User({
+        username,
+        email,
+        password: hashPass,
+      });
+      const result = await newUser.save();
+      const token = generateToken(result);
+      return { ...result._doc, token };
     },
   },
 };
